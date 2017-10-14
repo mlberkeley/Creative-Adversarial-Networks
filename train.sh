e4d0bd4eef014fd97a7c841a8ade1489fb6a74f5
Phillip Kravtsov <phillip@kravtsov.net>
2017-10-14 15:31:35 -0700

    reverted default epochs, added train.sh

---
+++ train.sh
@@ -0,0 +1,18 @@
+
+python3 main.py \
+--epoch 25 \
+--learning_rate .0002 \
+--beta1 0.5 \
+--batch_size 64 \
+--input_height 108 \
+--output_height 64 \
+--dataset wikiart/Impressionism \
+--input_fname_pattern *.jpg \
+--checkpint_dir checkpoint \
+--sample_dir samples \
+--train False \
+--crop False \
+--visualize False 
+
+
+-
