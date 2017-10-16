c787dc1c8901fcdaeb7eda1186438a044e82bc7a
Phillip <phillip@kravtsov.net>
2017-10-15 20:34:27 -0700

    hyperparameter changes

--- train.sh
+++ train.sh
@@ -1,11 +1,11 @@
 
 python3 main.py \
---epoch 25 \
+--epoch 5 \
 --learning_rate .0002 \
 --beta 0.5 \
---batch_size 8 \
---input_height 64 \
---output_height 64 \
+--batch_size 6 \
+--input_height 128 \
+--output_height 128 \
 --dataset wikiart \
 --input_fname_pattern */*/.jpg \
 --dataset wikiart \
