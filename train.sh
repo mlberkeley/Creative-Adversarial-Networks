9e100edd17c592f008295537f8e0cd8619d5f887
Phillip <phillip@kravtsov.net>
2017-11-01 22:05:13 -0700

    train.sh

--- train.sh
+++ train.sh
@@ -1,12 +1,12 @@
-export CUDA_VISIBLE_DEVICES=0
+export CUDA_VISIBLE_DEVICES=1
 python3 main.py \
 --epoch 25 \
 --learning_rate .0002 \
 --beta 0.5 \
 --batch_size 36 \
---input_height 64 \
---output_height 64 \
---dataset wikiart \
+--input_height 28 \
+--output_height 28 \
+--dataset mnist \
 --input_fname_pattern */*.jpg \
 --checkpoint_dir checkpoint \
 --sample_dir samples \
