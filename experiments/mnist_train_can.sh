0bcba40436d7b626fb6f224bc10cf2b3ead2df28
Phillip Kuznetsov <philkuz@berkeley.edu>
2017-11-08 11:55:31 -0800

    Minor commit Adding new experiments directory to contain the different experiments for reproducibility + changed way logs are made.

---
+++ experiments/mnist_train_can.sh
@@ -0,0 +1,19 @@
+export CUDA_VISIBLE_DEVICES=1
+python3 main.py \
+--epoch 25 \
+--learning_rate .0001 \
+--beta 0.5 \
+--batch_size 4 \
+--sample_size 9 \
+--input_height 28 \
+--output_height 28 \
+--lambda_val 1.0 \
+--smoothing 1 \
+--dataset mnist \
+--input_fname_pattern */*.jpg \
+--checkpoint_dir checkpoint \
+--sample_dir samples \
+--crop False \
+--visualize False \
+--can True \
+--train
