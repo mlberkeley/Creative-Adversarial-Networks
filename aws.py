fe41b29cb943bba09e004d1fe4e1b06ebd7feacc
Phillip Kuznetsov <philkuz@berkeley.edu>
2017-11-27 16:37:25 +0000

    Retabbed aws.py, added new experiment, and add "certain_upload" for logs.

--- aws.py
+++ aws.py
@@ -8,26 +8,28 @@ import os
 def bucket_exists(bucket):
   s3 = boto3.resource('s3')
   return s3.Bucket(bucket) in s3.buckets.all()
-def upload_path(local_directory, bucket, destination):
-	client = boto3.client('s3')
-	# enumerate local files recursively
-	for root, dirs, files in os.walk(local_directory):
+def upload_path(local_directory, bucket, destination, certain_upload=False):
+  client = boto3.client('s3')
+  # enumerate local files recursively
+  for root, dirs, files in os.walk(local_directory):
 
-		for filename in files:
+    for filename in files:
 
-			# construct the full local path
-			local_path = os.path.join(root, filename)
+      # construct the full local path
+      local_path = os.path.join(root, filename)
 
-			# construct the full Dropbox path
-			relative_path = os.path.relpath(local_path, local_directory)
-			s3_path = os.path.join(destination, relative_path)
+      # construct the full Dropbox path
+      relative_path = os.path.relpath(local_path, local_directory)
+      s3_path = os.path.join(destination, relative_path)
 
-			# relative_path = os.path.relpath(os.path.join(root, filename))
+      if certain_upload:
+        client.upload_file(local_path, bucket, s3_path)
+        return
 
-			print('Searching "%s" in "%s"' % (s3_path, bucket))
-			try:
-				client.head_object(Bucket=bucket, Key=s3_path)
-				# print("Path found on S3! Skipping %s..." % s3_path)
-			except:
-				print("Uploading %s..." % s3_path)
-				client.upload_file(local_path, bucket, s3_path)
+      print('Searching "%s" in "%s"' % (s3_path, bucket))
+      try:
+        client.head_object(Bucket=bucket, Key=s3_path)
+        # print("Path found on S3! Skipping %s..." % s3_path)
+      except:
+        print("Uploading %s..." % s3_path)
+        client.upload_file(local_path, bucket, s3_path)
