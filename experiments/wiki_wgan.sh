0a4149a1d45418f181d12df08282f8967368e430
Phillip <phillip@kravtsov.net>
2017-12-23 23:36:11 -0800

    small change

---
+++ experiments/wiki_wgan.sh
@@ -0,0 +1,22 @@
+export CUDA_VISIBLE_DEVICES=0
+python3 main.py \
+--epoch 25 \
+--learning_rate .0001 \
+--beta 0.5 \
+--batch_size 32 \
+--sample_size 36 \
+--input_height 128 \
+--output_height 128 \
+--lambda_val 1.0 \
+--smoothing 1.0 \
+--use_resize True \
+--dataset wikiart \
+--input_fname_pattern */*.jpg \
+--checkpoint_dir checkpoint \
+--sample_dir samples \
+--crop False \
+--visualize False \
+--replay False \
+--can False \
+--wgan True \
+--train
